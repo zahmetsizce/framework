4b42a8b42617229cab71ee6515dd68da2bd9ac0e
Nuh <nuhorun@msn.com>
2016-05-05 16:16:01 +0300

    servis sağlayıcı eklendi

---
+++ src/ZahmetsizceServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+	namespace Zahmetsizce;
+
+	use Illuminate\Support\ServiceProvider;
+
+	class ZahmetsizceServiceProvider extends ServiceProvider
+	{
+
+		function register() {
+
+		}
+
+		function boot() {
+			
+		}
+
+
+	}
