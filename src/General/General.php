a8e5888981b385e495ac9a90418f21c9405d34d0
Nuh <nuhorun@msn.com>
2016-05-05 16:22:29 +0300

    genel sınıf eklendi

---
+++ src/General/General.php
@@ -0,0 +1,32 @@
+<?php
+
+	namespace Zahmetsizce\General;
+
+	/*
+	 | ========================================================
+	 | GENERAL SINIFI
+	 | ========================================================
+	 | Bu sınıf bağlanıldığı sınıflarda kullanılacak
+	 | olan temel işlemleri gerçekleştirmekte kullanılacaktır.
+	 | ========================================================
+	 */
+	class General {
+
+		/**
+		 * 
+		 */
+		const VERSION = '1.0.0';
+		
+		/**
+		 * __call
+		 * 
+		 * @param  string $method	  Çağrılan methodun adı
+		 * @param  mixed  $parameters Çağrılan method için kullanılan parametreler
+		 * @return mixed
+		 */
+		public function __call($method, $parameters)
+		{
+
+		}
+
+	}
